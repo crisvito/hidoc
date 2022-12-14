559657af0f1d05e27fc60d8d2e0e99c0c11bd299
Pitokaii <crisvitoc@gmail.com>
2022-12-14 23:24:04 +0800

    chore : added more article

--- src/contents/article/artikel-terbaru.jsx
+++ src/contents/article/artikel-terbaru.jsx
@@ -8,18 +8,18 @@ export function ArtikelTerbaru() {
         <h1 className="text-2xl text-slate-800">Artikel Terbaru</h1>
         <div className="mt-10">
           <div className="bg-white">
-            <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
+            <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
               {dataArticle.map((data) => (
                 <Link
                   to={data.slug}
                   key={data.id}
-                  className="group rounded-lg flex flex-col gap-4"
+                  className="group rounded-lg flex flex-col gap-4 shadow-lg p-2"
                 >
-                  <div className="lg:h-64 h-48  rounded-md group-hover:opacity-75">
+                  <div className="h-46 rounded-md group-hover:opacity-75">
                     <img
                       src={data.urlImage}
                       alt={data.title}
-                      className="h-full w-full object-cover object-center"
+                      className="h-36 w-full object-cover object-center"
                     />
                   </div>
                   <h3 className="text-sm text-gray-700 font-medium text-center">
--- src/routes/article/$artikel.jsx
+++ src/routes/article/$artikel.jsx
@@ -11,19 +11,34 @@ export function ArtikelDetail() {
   return (
     <Layout>
       <div className="lg:w-3/4 md:w-4/5 sm:p-10 p-5 md:pl-20 w-full">
-        <div className="artikel-detail p-10 flex flex-col gap-10">
-          <h1 className="font-semibold text-3xl tracking-widest">
-            {item?.title}
-          </h1>
+        <div className="artikel-detail p-10 flex flex-col gap-8">
+          <span className="flex sm:flex-row flex-col sm:items-center items-start gap-6 justify-between">
+            <div className="flex items-center gap-3 text-blue-500">
+              <ArrowLeft size={30} />
+              <Link to="/artikel">Kembali ke Halaman Sebelumnya</Link>
+            </div>
+            <div className="flex flex-col sm:text-right text-left font-medium text-slate-700">
+              <span>{item?.articleDate}</span>
+              <span>{item?.articleTime}</span>
+            </div>
+          </span>
+          <div className="flex flex-col gap-3">
+            <h1 className="font-medium text-3xl tracking-widest leading-relaxed">
+              {item?.title}
+            </h1>
+            <span className="font-medium italic">"{item?.excerpt}"</span>
+            <div className="flex sm:flex-row flex-col sm:items-center items-start justify-end gap-3">
+              <span className="font-medium">Ditinjau oleh</span>
+              <span className="px-4 py-1 bg-blue-200 rounded text-blue-800">
+                {item?.confirmBy}
+              </span>
+            </div>
+          </div>
           <img
             src={item?.urlImage}
             alt={item?.title}
-            className="md:h-72 sm:h-60 h-52"
+            className="md:h-96 md:h-72 sm:h-60 h-56 bg-cover bg-center"
           />
-          <span className="flex items-center gap-3 text-blue-500">
-            <ArrowLeft size={30} />
-            <Link to="/artikel">Kembali ke Halaman Sebelumnya</Link>
-          </span>
           <div className="artikel-content flex flex-col gap-10 leading-loose tracking-wide">
             {parse(`${item?.content}`)}
             {parse(`${item?.content2}`)}
