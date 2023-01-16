48d07f671c9f03f7052890e33d1626746d58f0c3
Pitokaii <crisvitoc@gmail.com>
2023-01-16 19:38:56 +0800

    chore : add tailwind config for xs screens

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      xs: "475px",
+      ...defaultTheme.screens,
+    },
     extend: {},
   },
   darkMode: "class",
